5f5ef095288dc8c5e0b8f08753d26ecdfe40f546
张宇 <zhangyu@silverz.cc>
2017-03-28 00:57:41 +0800

    增加了抽取和重写技术，重写了虚工厂方法，并注入依赖。

---
+++ Chapter3/LogAn/LogAnalyzerVirtualFactory.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chapter3.LogAn;
+
+namespace LogAn
+{
+    public class LogAnalyzerVirtualFactory
+    {
+        /*
+        //被测代码
+        Virtual IExtensionManager getExtensionManager()
+        {
+            return 
+            new FileExtensionManager()  ///manager
+        }
+         
+        IsValidFileName(string)
+        {
+            IExtensionManager manager = getExtensionManager();
+            if(manager.isvalid(file))   ///manager
+            ...
+        }
+             
+        //测试代码
+        public IExtensionManager manager;
+
+        override IExtensionManager getExtensionManager()
+        {
+            return
+            manager  ///manager!!这里的Manager把new FileExtensionManager()替换掉了
+        }
+         
+        IsValidFileName(string)
+        {
+            IExtensionManager manager = getExtensionManager();
+            if(manager.isvalid(file))  ///manager
+            ...
+        }
+             
+         **/
+
+        //这个工厂方法，被定义为虚函数，以便后面重写并注入伪对象
+        protected virtual IExtensionManager GetManager()
+        {
+            return new FileExtensionManager();
+        }
+        public bool IsValidLogFileName(string fileName)
+        {
+            return GetManager().IsValid(fileName);
+        }
+    }
+
+    //这个类就是用来测试的类，其中最重要的部分就是把工厂重写了，可以通过构造函数给这个重写的类注入依赖
+    class LogAnalyzerVirtualFactoryOverride : LogAnalyzerVirtualFactory
+    {
+        //通过这个构造函数就可以把伪对象注入进来了
+        public LogAnalyzerVirtualFactoryOverride(IExtensionManager mgr)
+        {
+            manager = mgr;
+        }
+        private IExtensionManager manager;
+        //这里把原来类的工厂重写了，返回构造函数指定值
+        protected override IExtensionManager GetManager()
+        {
+            return manager;
+        }
+    }
+
+    //下面这个已经在外面定义好了，直接用就行了
+    //internal class FakeExtensionManager : IExtensionManager
+    //{
+    //    //模拟返回true或false用的，需要测试时赋值
+    //    public bool WillBeValid = false;
+
+    //    //模拟返回异常的，需要测试时赋值
+    //    public Exception WillThrow = null;
+    //    public bool IsValid(string fileName)
+    //    {
+    //        if (WillThrow != null) { throw WillThrow; }
+
+    //        return WillBeValid;
+    //    }
+    //}
+}
---
+++ Chapter3/LogAn/LogAnalyzerVirtualFactoryTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LogAn
+{
+    [TestFixture]
+    public class LogAnalyzerVirtualFactoryTest
+    {
+        [Test]
+        public void OverrideTest()
+        {
+            //设置要使用的桩，并赋值
+            FakeExtensionManager stub = new FakeExtensionManager();
+            stub.WillBeValid = true;
+
+            //创建被测试类的派生类实例
+            LogAnalyzerVirtualFactoryOverride logan = new LogAnalyzerVirtualFactoryOverride(stub);
+            bool result = logan.IsValidLogFileName("file.ext");
+            Assert.True(result);
+        }
+
+    }
+}
